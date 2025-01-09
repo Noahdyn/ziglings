8d4223120471d1d0677923750f703665dd272716
Dustin <dustin.tamalun@gmail.com>
2025-01-09 15:09:46 +0100

    Solve Exercise 105

--- exercises/105_threading2.zig
+++ exercises/105_threading2.zig
@@ -81,8 +81,8 @@ pub fn main() !void {
         defer handle1.join();
 
         // Second thread to calculate the minus numbers.
-        ???
-        
+        const handle2 = try std.Thread.spawn(.{}, thread_pi, .{ &pi_minus, 3, count });
+        defer handle2.join();
     }
     // Here we add up the results.
     std.debug.print("PI ≈ {d:.8}\n", .{4 + pi_plus - pi_minus});
@@ -94,6 +94,7 @@ fn thread_pi(pi: *f64, begin: u64, end: u64) !void {
         pi.* += 4 / @as(f64, @floatFromInt(n));
     }
 }
+
 // If you wish, you can increase the number of loop passes, which
 // improves the number of digits.
 //
